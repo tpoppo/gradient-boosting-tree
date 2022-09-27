28b57888ac5a38ecc1c31580f51d70683893c606
Lorenzo Rossi <30434380+tpoppo@users.noreply.github.com>
2022-09-27 10:41:06 +0200

    Add perf mode

---
+++ perf.sh
@@ -0,0 +1,11 @@
+echo "\n\n--------------------------------- STEP 1 ---------------------------------" &&
+cmake -S . -B build -DRELEASE=ON &&
+echo "\n\n--------------------------------- STEP 2 ---------------------------------" &&
+cmake --build build &&
+echo "\n\n--------------------------------- STEP 3 ---------------------------------" &&
+cd build &&
+sudo perf record ./tests &&
+sudo perf report > profiling-results.txt && 
+echo "Created build/profiling-results.txt"
+
+
